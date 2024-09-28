29cb273c8a415e817d978c455396e23346eb9ca2
honorine22 <higiranez@gmail.com>
2024-09-28 23:16:50 +0200

    app cpmplete

--- app/src/main/java/com/example/ampersand_assessment/MainActivity.kt
+++ app/src/main/java/com/example/ampersand_assessment/MainActivity.kt
@@ -9,11 +9,7 @@ import com.example.ampersand_assessment.ui.screens.ListViewActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val openListViewButton: AppCompatButton = findViewById(R.id.button_open_list_view)
-        openListViewButton.setOnClickListener {
             val intent = Intent(this, ListViewActivity::class.java)
             startActivity(intent)
-        }
     }
 }
